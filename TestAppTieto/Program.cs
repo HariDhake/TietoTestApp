9fac66930566b4f39b29c17a97f0da2864b6be42
xxdhakeh <xxdhakeh@WPF2KD14H>
2024-07-09 12:50:04 +0530

    Add project files.

---
+++ TestAppTieto/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestAppTieto;
+
+namespace TestAppTieto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello TietoEvry");
+            calculation calculation = new calculation();
+            Console.WriteLine("Addition ->"+ calculation.add(20,10));
+            Console.WriteLine("Substraction ->"+ calculation.subtract(20,10));
+            Console.Read();
+        }
+    }
+}
